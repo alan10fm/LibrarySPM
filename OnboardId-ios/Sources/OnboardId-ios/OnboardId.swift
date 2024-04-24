dd0fd116a94afdb6ff66256b54c8ae26f43dac96
“alan10fm” <alan10fm@gmail.com>
2024-04-24 12:42:04 -0400

    asdasd

---
+++ OnboardId-ios/Sources/OnboardId-ios/OnboardId.swift
@@ -0,0 +1,11 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+public class OnboardId {
+    
+    public init() {}
+    
+    public func isConnected() -> Bool {
+        return true
+    }
+}
