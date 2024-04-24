037620f8190097291fdf22624f4541461aa63662
“alan10fm” <alan10fm@gmail.com>
2024-04-24 15:59:07 -0400

    asdasd

--- Package.swift
+++ Package.swift
@@ -11,22 +11,16 @@ let package = Package(
             name: "LibrarySPM",
             targets: ["LibrarySPM"]),
     ],
-    dependencies: [
-        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.9.1")),
-        .package(url: "https://github.com/getsentry/sentry-cocoa", .upToNextMajor(from: "8.24.0")),
-        .package(url: "https://github.com/devicekit/DeviceKit.git", .upToNextMajor(from: "4.0.0"))
-    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "LibrarySPM",
-            dependencies: [
-                .product(name: "Sentry", package: "sentry-cocoa"),
-                "Alamofire",
-                "DeviceKit",
-            ]
+            name: "LibrarySPM"
         ),
+        .binaryTarget(
+               name: "LibraryPackage",
+               path: "xcframework/LibraryPackage.xcframework"
+       ),
         .testTarget(
             name: "LibrarySPMTests",
             dependencies: ["LibrarySPM"]),
