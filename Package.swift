45210b6ad26523b753cd503b19c76d0e2dc145f6
“alan10fm” <alan10fm@gmail.com>
2024-04-24 14:59:43 -0400

    asdasdasd

--- Package.swift
+++ Package.swift
@@ -9,23 +9,23 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "LibrarySPM",
-            targets: ["LibrarySPM", "NetkiSDK"]),
+            targets: ["LibrarySPM"]),
     ],
-//    dependencies: [
-//        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.9.1")),
-//        .package(url: "https://github.com/getsentry/sentry-cocoa", .upToNextMajor(from: "8.24.0")),
-//        .package(url: "https://github.com/devicekit/DeviceKit.git", .upToNextMajor(from: "4.0.0"))
-//    ],
+    dependencies: [
+        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.9.1")),
+        .package(url: "https://github.com/getsentry/sentry-cocoa", .upToNextMajor(from: "8.24.0")),
+        .package(url: "https://github.com/devicekit/DeviceKit.git", .upToNextMajor(from: "4.0.0"))
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "LibrarySPM"
-//            dependencies: [
-//                .product(name: "Sentry", package: "sentry-cocoa"),
-//                "Alamofire",
-//                "DeviceKit",
-//            ]
+            name: "LibrarySPM",
+            dependencies: [
+                .product(name: "Sentry", package: "sentry-cocoa"),
+                "Alamofire",
+                "DeviceKit",
+            ]
         ),
         .binaryTarget(
             name: "NetkiSDK",
--- Sources/LibrarySPM/LibrarySPM.swift
+++ Sources/LibrarySPM/LibrarySPM.swift
@@ -1,10 +1,9 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 //
-//import Alamofire
-//import Sentry
-//import DeviceKit
-//import NetkiSDK
+import Alamofire
+import Sentry
+import DeviceKit
 
 public class LibrarySPM {
     
@@ -14,7 +13,7 @@ public class LibrarySPM {
         return "Yes!"
     }
     
-//    public func versions() -> String {
-//        return "Alamo: \(Alamofire.AFInfo.version), Sentry: \(SentrySDK.version()), DeviceKit: \(Device.current)"
-//    }
+    public func versions() -> String {
+        return "Alamo: \(Alamofire.AFInfo.version), Sentry: \(SentrySDK.version()), DeviceKit: \(Device.current)"
+    }
 }
