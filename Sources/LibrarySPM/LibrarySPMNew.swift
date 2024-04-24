cdd2c0753c1c83010f0edd6ba3c977b176d8905d
“alan10fm” <alan10fm@gmail.com>
2024-04-24 15:59:07 -0400

    asdasd

---
+++ Sources/LibrarySPM/LibrarySPMNew.swift
@@ -0,0 +1,12 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+//
+
+public class LibrarySPMNew {
+    
+    public init() {}
+    
+    public func isPackagePackaging() -> String {
+        return "Yes!"
+    }
+}
